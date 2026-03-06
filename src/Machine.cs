d89df32f948146bc4596cfd0156e14f1a68196fb
Mark-James McDougall <contact@markjames.dev>
2026-03-06 01:25:57 -0500

    Load ROMs into memory.

--- src/Machine.cs
+++ src/Machine.cs
@@ -1,9 +1,16 @@
+using Malus.Devices;
 using Velutia.Processor;
 
 namespace Malus;
 
 public class Machine
 {
+    // ProDOS HDC paravirtualization hook
+    private const ushort ProdosHdcRomStart = 0xC700;
+    
+    // Apple II+ with Integer ROM Language Card
+    private const ushort Apple2PlusRomStart = 0xD000;
+    
     public Machine()
     {
         
@@ -12,10 +19,10 @@ public class Machine
     public void Start()
     {
         var memory = new Memory();
-        
-        // Load Apple II+ ROM at $D000 - $FFFF
         var a2Rom = RomLoader.LoadRom("apple2p.rom");
-        memory.Load(a2Rom, 0xD000);
+        
+        memory.Load(a2Rom, Apple2PlusRomStart);
+        memory.Load(ProDosHdcRom.ProDosHdcRomMap, ProdosHdcRomStart);
         
         var bus = new Bus(memory);
         var cpu = new Cpu(bus);
